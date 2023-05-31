3947a2025991b92c8abfa9d6d9599876e6497ee8
Monique Day <admin@daedreamstudio.com.au>
2023-05-31 22:05:23 +1000

    xfgcn

--- scripts.js
+++ scripts.js
@@ -12,3 +12,15 @@ $(document).ready(function() {
 
 });
 
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+    
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+    
+});
+
